102c169f4f8036b42a76dced646717d538544fd0
hector0820 <hector.hdez.08@gmail.com>
2022-11-25 15:55:23 -0600

    Adding rust scripts from microsoft learn course

---
+++ Courses/rust-learning-pathr/hello-cargo/target/debug/deps/hello_cargo-5a03a1324d3b74c0.d
@@ -0,0 +1,5 @@
+/home/hector/Documents/Scripts/rust learning path/hello cargo/target/debug/deps/hello_cargo-5a03a1324d3b74c0: src/main.rs
+
+/home/hector/Documents/Scripts/rust learning path/hello cargo/target/debug/deps/hello_cargo-5a03a1324d3b74c0.d: src/main.rs
+
+src/main.rs:
---
+++ Courses/rust-learning-pathr/hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/home/hector/Documents/Scripts/rust\ learning\ path/hello\ cargo/target/debug/hello-cargo: /home/hector/Documents/Scripts/rust\ learning\ path/hello\ cargo/src/main.rs
